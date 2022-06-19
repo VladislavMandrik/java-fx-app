9a0b34091c062c0fe053b7b5bb71b8ec3f0af9d6
VladislavMandrik <ladvdrikman@gmail.com>
2022-06-19 17:20:04 +0300

    addded homework7

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,7 +2,6 @@ module ru.gb.javafxapplesson4 {
     requires javafx.controls;
     requires javafx.fxml;
 
-
-    opens ru.gb.javafxapplesson4 to javafx.fxml;
-    exports ru.gb.javafxapplesson4;
+    exports ru.gb.javafxapplesson4.client;
+    opens ru.gb.javafxapplesson4.client to javafx.fxml;
 }
